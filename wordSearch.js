f37c3afe0b1d828d960f11fdc6ff30f09938ea4b
Erin Fung <erinfungg@gmail.com>
2020-04-19 22:33:00 -0700

    wordSearch

--- wordSearch.js
+++ wordSearch.js
@@ -1,3 +1,7 @@
+/*Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.*/
+
 function exist(board, word) {
   let str = word;
   for (let r = 0; r < board.length; r++) {
