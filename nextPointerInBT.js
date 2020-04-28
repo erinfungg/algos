015e79c7663673a37dc7b5a17aeb5b30db464bd8
Erin Fung <erinfungg@gmail.com>
2020-04-27 21:36:25 -0700

    completed nextPointerInBT

---
+++ nextPointerInBT.js
@@ -0,0 +1,22 @@
+/*You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ */
+
+var connect = function(root) {
+  if (!root || !root.left) return root;
+  root.left.next = root.right;
+  root.right.next = root.next ? root.next.left : null;
+  connect(root.left);
+  connect(root.right);
+  return root;
+};
